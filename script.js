f6daa7ae849a50a084ec7dd5f7f37baa7f3d381a
Otavio Brito <otaviofbrito@gmail.com>
2022-02-06 14:59:40 -0300

    fetching playermatchs

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+const api_Key = 'RGAPI-20c3afac-f298-4331-aaed-48a2b8a3d302'
 
 const Form = {
 
@@ -8,7 +8,7 @@ const Form = {
   
 
   async getPlayerInfo(){
-    let playerUrl = `https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${Form.playerName}?api_key=`
+    let playerUrl = `https://br1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${Form.playerName}?api_key=${api_Key}`
     
     try {
       let res = await fetch(playerUrl)
@@ -19,21 +19,58 @@ const Form = {
     }
   },
 
-  async renderPlayerInfo(){
+  async renderPlayerUUID(){
 
     let player = await Form.getPlayerInfo()
     let puuid = player.puuid
-    let name = player.name
-    let playerIcon = player.profileIconId
     return {
       puuid,
-      name,
-      playerIcon
     }
-  }
-  
+  },
 
+
+  async fetchMatches() {
+    let {puuid} = await Form.renderPlayerUUID()
+    let matchUrl = `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${puuid}/ids?start=0&count=5&api_key=${api_Key}`
+
+    try {
+      let res = await fetch(matchUrl)
+      
+      return await res.json()
+    } catch (error) {
+      console.log(error)
+    }
+  },
+
+  async fetchShortHand(){
+    let matches = await Form.fetchMatches()
+    let matchArray = []
+    matches.forEach(match => {
+
+      let matchesUrl = `https://americas.api.riotgames.com/lol/match/v5/matches/${match}?api_key=${api_Key}`
+     
+      matchArray.push(matchesUrl)
+    })
+    return matchArray
+  },
+
+  async matchInfos(){
+    let matchArray = await Form.fetchShortHand()
+    let matchInfoArray = []
+    matchArray.forEach(async arr => {
+      let res = await fetch(arr).then(resp => resp.json())
+      
+      matchInfoArray.push(res)
+    })
+    return matchInfoArray
+ },
+
+ async renderMatches(){
+    let matches = await Form.matchInfos()
+    let test = matches[0]
+   console.log(test)
+ }
 }
 
 
-console.log()
+Form.renderMatches()
